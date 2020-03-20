4c0fd1404b327086d855f4c395fa536f7b6c501e
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-03-19 21:05:53 -0400

    MVP reached!

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express');
-
+const express = require("express");
 const server = express();
+const userRoutes = require("./users/userRouter");
+const postsRoutes = require("./posts/postRouter");
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.use(express.json());
+
+server.listen(5000, () => {
+  console.log("\n*** Server running on Port 5000! ***\n");
 });
 
-//custom middleware
 
-function logger(req, res, next) {}
+//custom middleware
+function logger(req, res, next) {
+  console.log(`A ${req.method} was sent at ${Date.now()} to ${req.originalUrl}!`);
+  next();
+}
+
+server.use(logger);
+server.use("/users", userRoutes);
+server.use("/posts", postsRoutes);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
